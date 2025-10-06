81be5eead4275c7db24bec91e63381f39f0bce1a
avivlosangeles-design <avivlosangeles@gmail.com>
2025-10-06 00:31:43 -0700

    get-map-key.js

--- get-map-key.js
+++ get-map-key.js
@@ -1,1 +1,10 @@
-
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/plain",
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: process.env.AZURE_MAPS_KEY
+  };
+};
